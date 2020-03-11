86de16f2f97371b6051dfe846cd733c8396cf8df
power <trusionp@gmail.com>
2020-03-10 22:54:12 -0600

    auto reset

--- src/index.ts
+++ src/index.ts
@@ -5,80 +5,90 @@ import bp from "body-parser";
 import express_user_ip from "express-ip";
 //?  decorators metadata
 import "reflect-metadata";
+import cluster from "cluster";
 
 import connectDB from "./DB/index";
 import { RegisterResolver } from "./resolvers/UserResolver";
 import { resolveUserReference, User } from "./schema/UserSchema";
 
 const PORT: string = process.env.PORT || "3000";
+if (cluster.isMaster) {
+  cluster.fork();
 
-(async () => {
-  try {
-    // Initialize the app
-    const app = express();
-    app.use(
-      /\/((?!graphql).)*/,
-      bp.urlencoded({
-        limit: "50mb",
-        extended: true
-      })
-    );
-    app.use(
-      /\/((?!graphql).)*/,
-      bp.json({
-        limit: "50mb"
-      })
-    );
+  cluster.on("exit", function(worker, code, signal) {
+    cluster.fork();
+  });
+}
 
-    app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
-
-    app.use((req, res, next) => {
-      res.setHeader("Access-Control-Allow-Origin", "*"); //* dominios por donde se permite el acceso
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "POST,GET,DELETE,UPDATE,PUT"
-      ); //* metodos permitidos por el cliente
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type,Authorization"
+if (cluster.isWorker) {
+  (async () => {
+    try {
+      // Initialize the app
+      const app = express();
+      app.use(
+        /\/((?!graphql).)*/,
+        bp.urlencoded({
+          limit: "50mb",
+          extended: true
+        })
       );
-      //* dominios por donde se permite el acceso
-      //* graph ql no envia una respuesta valida con el tipo options, cuando hay un tipo de request OPTIONS se retorna una respuesta con el estado 200
-      // * graphql does not send a valid response when the OPTIONS request is received, if a OPTIONS request type is presented server return an empty response with the code 200
-      if (req.method === "OPTIONS") {
-        res.sendStatus(200);
-      }
-      next();
-    });
+      app.use(
+        /\/((?!graphql).)*/,
+        bp.json({
+          limit: "50mb"
+        })
+      );
 
-    const server = new ApolloServer({
-      schema: await buildFederatedSchema(
-        {
-          resolvers: [RegisterResolver],
-          orphanedTypes: [User]
-        },
-        {
-          User: { __resolveReference: resolveUserReference }
+      app.use(express_user_ip().getIpInfoMiddleware); //* get the user location data
+
+      app.use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*"); //* dominios por donde se permite el acceso
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "POST,GET,DELETE,UPDATE,PUT"
+        ); //* metodos permitidos por el cliente
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "Content-Type,Authorization"
+        );
+        //* dominios por donde se permite el acceso
+        //* graph ql no envia una respuesta valida con el tipo options, cuando hay un tipo de request OPTIONS se retorna una respuesta con el estado 200
+        // * graphql does not send a valid response when the OPTIONS request is received, if a OPTIONS request type is presented server return an empty response with the code 200
+        if (req.method === "OPTIONS") {
+          res.sendStatus(200);
         }
-      ),
-      context: ({ req }) => {
-        return req;
-      },
-      formatError: err => {
-        return err;
-      }
-    });
-    // The GraphQL endpoint
+        next();
+      });
 
-    server.applyMiddleware({ app, path: "/graphql" });
+      const server = new ApolloServer({
+        schema: await buildFederatedSchema(
+          {
+            resolvers: [RegisterResolver],
+            orphanedTypes: [User]
+          },
+          {
+            User: { __resolveReference: resolveUserReference }
+          }
+        ),
+        context: ({ req }) => {
+          return req;
+        },
+        formatError: err => {
+          return err;
+        }
+      });
+      // The GraphQL endpoint
 
-    // Start the server
-    await connectDB();
+      server.applyMiddleware({ app, path: "/graphql" });
 
-    app.listen(PORT, () => {
-      console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})();
+      // Start the server
+      await connectDB();
+
+      app.listen(PORT, () => {
+        console.log(`Go to http://localhost:${PORT}/graphiql to run queries!`);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+}
